5eb32e036dd49f6f159cb19bce27ad6ce1047780
Huynh Thi Thao Minh <huynhthithaominhh0802@gmail.com>
2026-06-04 16:23:22 +0700

    fix: xoa bo hoan toan file interface tai khoan bi trung lap trong models

--- Backend/Models/ITaiKhoanDAL.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Interfaces
-{
-    public interface ITaiKhoanDAL
-    {
-        bool KiemTraDangNhap(string tenDangNhap, string matKhau);
-        bool CoTaiKhoanNaoChua();
-        bool TaoTaiKhoan(Models.TaiKhoan tk);
-    }
-}
